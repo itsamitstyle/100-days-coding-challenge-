45df85e8de178bd9cdb058ba91b305a3106a1fff
amit kumar <104476740+itsamitstyle@users.noreply.github.com>
2022-12-17 16:06:31 +0530

    Create chapter1.py

---
+++ chapter1.py
@@ -0,0 +1,38 @@
+# module comment and pip 
+# what is a module 
+# module is a file container that store the somebody written 
+# thre are two types of module in python =  1 = build_in_module and external module 
+# pip is a package manager for python that can install any type of library using pip 
+# comment = sometimes you write a code but you dont exexute the costumer so youcan use comment = single line coment and multi line commment 
+
+# create a first python program 
+
+
+print("hello world ");
+
+
+
+# CHAPTER 1 PRACTICE SET 
+
+# 1) write a program to print twinkle twinkle little star poem in python .
+
+print ('''SONG LYRICS
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''');
+
+
+# 2) use repl toprint table 5 on it 
+
+
+
